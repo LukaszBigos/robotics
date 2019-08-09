ab49c5b0a9c6771d102abbec45d26354c57f238f
LukaszBigos <lukaszbi@herbalife.com>
2019-08-09 15:24:47 +0200

    converting to redux

---
+++ src/actions.js
@@ -0,0 +1,24 @@
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from "./constants";
+
+export const setSearchField = text => ({
+  type: CHANGE_SEARCH_FIELD,
+  payload: text
+});
+
+export const requestRobots = () => async dispatch => {
+  dispatch({
+    type: REQUEST_ROBOTS_PENDING
+  });
+  try {
+    const res = await fetch("https://api.github.com/users");
+    const data = await res.json();
+    dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error });
+  }
+};
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,48 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.css";
 
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import { setSearchField, requestRobots } from "../actions";
 
-export default class App extends Component {
-  state = {
-    robots: [],
-    searchTerm: ""
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  };
+};
+
+class App extends Component {
 
   componentDidMount() {
-    this.getUsers();
-    console.log(this.state.robots);
+    this.props.onRequestRobots();
   }
 
-  getUsers = async () => {
-    try {
-      const res = await fetch("https://api.github.com/users");
-      const data = await res.json();
-      this.setState({
-        robots: data
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  onSearchChange = e => {
-    this.setState({
-      searchTerm: e.target.value
-    });
-  };
-
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
+    const { searchField, onSearchChange, robots, isPending } = this.props;
+    const filteredRobots = robots.filter(robot => {
       return robot.login
         .toLowerCase()
-        .includes(this.state.searchTerm.toLowerCase());
+        .includes(searchField.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
+    if (isPending) {
       return <h1>Loading robots...</h1>;
     } else {
       return (
         <div className="app-wrapper">
           <h1>Robotics</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
@@ -55,3 +51,8 @@ export default class App extends Component {
     }
   }
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
